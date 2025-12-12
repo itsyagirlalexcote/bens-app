60a50964e30039c92f700965207604fcfdabf6bf
Alexandra (Personal) <alexandrapersonal@alexandras-air.home>
2025-12-12 17:46:47 -0500

    Configure Vite base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+    base: '/bens-app/',
+    plugins: [react()],
   server: {
     host: true,
     port: 5173,
